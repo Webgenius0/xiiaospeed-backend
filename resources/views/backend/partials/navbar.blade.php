fbaef961870685d6aa459e03c55e8c6328b70778
mdiktushar <md.tushar1804@gmail.com>
2025-04-28 11:14:09 +0600

    about in bavbar

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -4,8 +4,8 @@
     <div class="navbar-vertical navbar nav-dashboard">
         <div class="h-100" data-simplebar>
             <!-- Brand logo -->
-            <a class="navbar-brand" href="{{route('dashboard')}}">
-                <img src="{{ asset('assets/backend/images/brand/logo/logo-2.svg')}}"
+            <a class="navbar-brand" href="{{ route('dashboard') }}">
+                <img src="{{ asset('assets/backend/images/brand/logo/logo-2.svg') }}"
                     alt="dash ui - bootstrap 5 admin dashboard template">
             </a>
             <!-- Navbar nav -->
@@ -13,19 +13,21 @@
 
                 <!-- Nav item -->
                 <li class="nav-item">
-                    <a class="nav-link {{Route::is('dashboard') ? 'active': ''}}" href="{{route('dashboard')}}">
+                    <a class="nav-link {{ Route::is('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">
                         <i data-feather="home" class="nav-icon me-2 icon-xxs"></i>Dashboard
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link {{Route::is('v1.setting.mail.show') ? 'active': ''}}" href="{{route('v1.setting.mail.show')}}">
-                        <i data-feather="mail" class="nav-icon me-2 icon-xxs"></i> Email Setting
+                    <a class="nav-link {{ Route::is('v1.about.show') ? 'active' : '' }}"
+                        href="{{ route('v1.about.show') }}">
+                        <i data-feather="mail" class="nav-icon me-2 icon-xxs"></i>About Section
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link {{Route::is('v1.setting.mail.show') ? 'active': ''}}" href="{{route('v1.setting.mail.show')}}">
+                    <a class="nav-link {{ Route::is('v1.setting.mail.show') ? 'active' : '' }}"
+                        href="{{ route('v1.setting.mail.show') }}">
                         <i data-feather="mail" class="nav-icon me-2 icon-xxs"></i> Email Setting
                     </a>
                 </li>
