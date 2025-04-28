33f7694ebc5e27c75c89ee2e013c3029eb32a4e2
mdiktushar <md.tushar1804@gmail.com>
2025-04-28 16:36:23 +0600

    route created

--- routes/web/v1/section.php
+++ routes/web/v1/section.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Web\V1\AboutController;
 use App\Http\Controllers\Web\V1\BrandController;
 use App\Http\Controllers\Web\V1\DesignController;
 use App\Http\Controllers\Web\V1\DevelopController;
+use App\Http\Controllers\Web\V1\MarketController;
+use App\Http\Controllers\Web\V1\SupportController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('about')->name('v1.about.')->controller(AboutController::class)
@@ -27,6 +29,20 @@ Route::prefix('brand')->name('v1.brand.')->controller(BrandController::class)
 
 
 Route::prefix('develop')->name('v1.develop.')->controller(DevelopController::class)
+->group(function () {
+    Route::get('/', 'show')->name('show');
+    Route::post('/', 'store')->name('store');
+});
+
+
+Route::prefix('market')->name('v1.market.')->controller(MarketController::class)
+->group(function () {
+    Route::get('/', 'show')->name('show');
+    Route::post('/', 'store')->name('store');
+});
+
+
+Route::prefix('support')->name('v1.support.')->controller(SupportController::class)
 ->group(function () {
     Route::get('/', 'show')->name('show');
     Route::post('/', 'store')->name('store');
