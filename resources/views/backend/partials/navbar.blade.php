6c3f8ba955017be2b890aef470d8a56aa4cf3423
mdiktushar <md.tushar1804@gmail.com>
2025-04-28 11:31:49 +0600

    about icon navbar

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -21,7 +21,23 @@
                 <li class="nav-item">
                     <a class="nav-link {{ Route::is('v1.about.show') ? 'active' : '' }}"
                         href="{{ route('v1.about.show') }}">
-                        <i data-feather="mail" class="nav-icon me-2 icon-xxs"></i>About Section
+                        <div style="margin-right: 8px">
+                            <svg fill="{{ Route::is('v1.about.show') ? '#624bff' : '#a3aab4' }}" width="14px" viewBox="0 0 128 128" id="Layer_1" version="1.1"
+                                xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <g>
+                                        <path
+                                            d="M64,1C29.3,1,1,29.3,1,64s28.3,63,63,63s63-28.3,63-63S98.7,1,64,1z M64,119C33.7,119,9,94.3,9,64S33.7,9,64,9 s55,24.7,55,55S94.3,119,64,119z">
+                                        </path>
+                                        <rect height="40" width="8" x="60" y="54.5"></rect>
+                                        <rect height="8" width="8" x="60" y="35.5"></rect>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>About Section
                     </a>
                 </li>
 
