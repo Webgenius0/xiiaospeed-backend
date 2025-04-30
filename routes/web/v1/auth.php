8cf4ff81b3e29c572f891e5e2b966a02e3d032a0
mdiktushar <md.tushar1804@gmail.com>
2025-04-30 12:26:20 +0600

    route close

--- routes/web/v1/auth.php
+++ routes/web/v1/auth.php
@@ -12,8 +12,8 @@ use App\Http\Controllers\Web\V1\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    // Route::get('register', [RegisteredUserController::class, 'create'])
-    //     ->name('register');
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
     // Route::post('register', [RegisteredUserController::class, 'store']);
 
