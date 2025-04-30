48f6a53a5ef940e0ab0588d463af597ec392f28c
mdiktushar <md.tushar1804@gmail.com>
2025-04-30 10:19:12 +0600

    navbar setup for comment

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -62,20 +62,23 @@
                         </div>Introduction
                     </a>
                 </li>
-                {{-- about --}}
+                {{-- project --}}
                 <li class="nav-item">
                     <a class="nav-link {{ Route::is('v1.project.*') ? 'active' : '' }}"
                         href="{{ route('v1.project.index') }}">
                         <div style="margin-right: 8px">
                             <svg viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" width="14px"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" fill="{{ Route::is('v1.project.*') ? '#624bff' : '#a3aab4' }}">
+                                xmlns:xlink="http://www.w3.org/1999/xlink"
+                                fill="{{ Route::is('v1.project.*') ? '#624bff' : '#a3aab4' }}">
                                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                                 <g id="SVGRepo_iconCarrier">
                                     <title>project-configuration</title>
                                     <g id="Page-1" stroke="none" stroke-width="1" fill="none"
                                         fill-rule="evenodd">
-                                        <g id="icon" fill="{{ Route::is('v1.project.*') ? '#624bff' : '#a3aab4' }}" transform="translate(42.666667, 42.666667)">
+                                        <g id="icon"
+                                            fill="{{ Route::is('v1.project.*') ? '#624bff' : '#a3aab4' }}"
+                                            transform="translate(42.666667, 42.666667)">
                                             <path
                                                 d="M277.333333,234.666667 L277.333,255.999667 L298.666667,256 L298.666667,298.666667 L277.333,298.666667 L277.333333,426.666667 L256,426.666667 L256,298.666667 L234.666667,298.666667 L234.666667,256 L256,255.999667 L256,234.666667 L277.333333,234.666667 Z M341.333333,234.666667 L341.333,341.332667 L362.666667,341.333333 L362.666667,384 L341.333,383.999667 L341.333333,426.666667 L320,426.666667 L320,383.999667 L298.666667,384 L298.666667,341.333333 L320,341.332667 L320,234.666667 L341.333333,234.666667 Z M405.333333,234.666667 L405.333,277.332667 L426.666667,277.333333 L426.666667,320 L405.333,319.999667 L405.333333,426.666667 L384,426.666667 L384,319.999667 L362.666667,320 L362.666667,277.333333 L384,277.332667 L384,234.666667 L405.333333,234.666667 Z M170.666667,7.10542736e-15 L341.333333,96 L341.333,213.333 L298.666,213.333 L298.666,138.747 L192,200.331 L192,323.018 L213.333,311.018 L213.333333,320 L234.666667,320 L234.666,348 L170.666667,384 L3.55271368e-14,288 L3.55271368e-14,96 L170.666667,7.10542736e-15 Z M42.666,139.913 L42.6666667,263.04 L149.333,323.022 L149.333,201.497 L42.666,139.913 Z M170.666667,48.96 L69.246,105.991 L169.656,163.963 L271.048,105.424 L170.666667,48.96 Z"
                                                 id="Combined-Shape"> </path>
@@ -84,6 +87,34 @@
                                 </g>
                             </svg>
                         </div>Project
+                    </a>
+                </li>
+                {{-- comment --}}
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('v1.comment.*') ? 'active' : '' }}"
+                        href="{{ route('v1.comment.index') }}">
+                        <div style="margin-right: 8px">
+                            <svg viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" width="14px"
+                                xmlns:xlink="http://www.w3.org/1999/xlink"
+                                xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="{{ Route::is('v1.comment.*') ? '#624bff' : '#a3aab4' }}">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <title>comment-2</title>
+                                    <desc>Created with Sketch Beta.</desc>
+                                    <defs> </defs>
+                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
+                                        sketch:type="MSPage">
+                                        <g id="Icon-Set" sketch:type="MSLayerGroup"
+                                            transform="translate(-152.000000, -255.000000)" fill="{{ Route::is('v1.comment.*') ? '#624bff' : '#a3aab4' }}">
+                                            <path
+                                                d="M168,281 C166.832,281 165.704,280.864 164.62,280.633 L159.912,283.463 L159.975,278.824 C156.366,276.654 154,273.066 154,269 C154,262.373 160.268,257 168,257 C175.732,257 182,262.373 182,269 C182,275.628 175.732,281 168,281 L168,281 Z M168,255 C159.164,255 152,261.269 152,269 C152,273.419 154.345,277.354 158,279.919 L158,287 L165.009,282.747 C165.979,282.907 166.977,283 168,283 C176.836,283 184,276.732 184,269 C184,261.269 176.836,255 168,255 L168,255 Z M175,266 L161,266 C160.448,266 160,266.448 160,267 C160,267.553 160.448,268 161,268 L175,268 C175.552,268 176,267.553 176,267 C176,266.448 175.552,266 175,266 L175,266 Z M173,272 L163,272 C162.448,272 162,272.447 162,273 C162,273.553 162.448,274 163,274 L173,274 C173.552,274 174,273.553 174,273 C174,272.447 173.552,272 173,272 L173,272 Z"
+                                                id="comment-2" sketch:type="MSShapeGroup"> </path>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>Comment
                     </a>
                 </li>
                 {{-- about --}}
